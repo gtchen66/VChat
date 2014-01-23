24e8c55ec22baf9108db14c61c79e616956e8ee5
gtchen66 <gtchen66@yahoo.com>
2014-01-22 19:48:51 -0800

    Initial Commit

---
+++ VChat/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  VChat
+//
+//  Created by George Chen on 1/22/14.
+//  Copyright (c) 2014 George Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
