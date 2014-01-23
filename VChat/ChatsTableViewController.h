bd75c915b80b6d2732612dbb71bdb4e7331e1934
gtchen66 <gtchen66@yahoo.com>
2014-01-22 23:51:59 -0800

    Files and .xib for other screens
    Added .h,.m,.xib files for Chats, Friends, Chatting, Profile, and
    Nearby screens

---
+++ VChat/ChatsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ChatsTableViewController.h
+//  VChat
+//
+//  Created by George Chen on 1/22/14.
+//  Copyright (c) 2014 George Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChatsTableViewController : UITableViewController
+
+@end
---
+++ VChat/FriendsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  FriendsTableViewController.h
+//  VChat
+//
+//  Created by George Chen on 1/22/14.
+//  Copyright (c) 2014 George Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FriendsTableViewController : UITableViewController
+
+@end
