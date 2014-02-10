2a427719df4836f5845cb49556d6cb82bb5c0cb0
gtchen66 <gtchen66@yahoo.com>
2014-02-10 03:10:28 -0800

    saving to parse and then retrieving

--- VChat/ChattingViewController.m
+++ VChat/ChattingViewController.m
@@ -8,11 +8,16 @@
 
 #import "ChattingViewController.h"
 
-@interface ChattingViewController ()
+@interface ChattingViewController () {
+    dispatch_queue_t myQueue;
+}
+
 @property (weak, nonatomic) IBOutlet UIButton *pushToRecordButton;
+@property (nonatomic, strong) NSMutableArray *latestRecordingArray;
 
 @property float startRecordingTime;
 @property float endRecordingTime;
+@property int indexForPlayback;
 
 // @property (nonatomic, strong) PFUser *user;
 
@@ -20,7 +25,11 @@
 - (IBAction)onUpPushToTalk:(id)sender;
 - (IBAction)onPlayButton:(id)sender;
 - (IBAction)onSendButton:(id)sender;
+
+- (IBAction)onTestButton:(id)sender;
 @end
+
+NSString* const RECORDING_CLASSNAME = @"UserRecording";
 
 @implementation ChattingViewController
 
@@ -33,6 +42,10 @@
 //        self.localUser = [[PFUser alloc]init];
         self.localUser = [PFUser currentUser];
         self.remoteUser = [[PFUser alloc]init];
+        
+//        [self.pushToRecordButton setTitle:@"Release to Stop" forState:UIControlStateHighlighted];
+//        [self.pushToRecordButton setTitle:@"Hold to Record" forState:UIControlStateNormal];
+
     }
     return self;
 }
@@ -69,6 +82,9 @@
     NSError *error = nil;
     
     audioRecorder = [[AVAudioRecorder alloc] initWithURL:soundFileURL settings:recordSettings error:&error];
+
+    [self.pushToRecordButton setTitle:@"Release to Stop" forState:UIControlStateHighlighted];
+    [self.pushToRecordButton setTitle:@"Hold to Record" forState:UIControlStateNormal];
     
 }
 
@@ -85,7 +101,7 @@
 
 - (IBAction)onDownPushToTalk:(id)sender {
     // Started holding button
-    self.pushToRecordButton.titleLabel.text = @"Release to stop";
+//    self.pushToRecordButton.titleLabel.text = @"Release to stop";
     self.startRecordingTime = [audioRecorder deviceCurrentTime];
     NSLog(@"Recording to %@ at time %.2f",[audioRecorder url],self.startRecordingTime);
     
@@ -105,20 +121,20 @@
     audioPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:audioRecorder.url error:nil];
     audioPlayer.delegate = self;
     [audioPlayer play];
+    NSLog(@"Playback routine started");
 }
 
 - (IBAction)onSendButton:(id)sender {
     // Okay to send...
-    NSLog(@"Sending to Parse... but first convert to base64");
     NSData *recordingData = [[NSData alloc] initWithContentsOfURL:audioRecorder.url];
-    NSLog(@"length of data is %d",recordingData.length);
+    NSLog(@"Sending to Parse.  Length of data is %d",recordingData.length);
 
     // Seems that parse can handle binary data.  That's good because
     // the data would be smaller then.
 //    NSString *encodedString = [nsdata base64EncodedStringWithOptions:0];
 //    NSLog(@"length of encoded string is %d",[encodedString length]);
 
-    NSString *recordingName = [[NSString alloc] initWithFormat:@"%@_%@_%.2f",self.localUser.username, self.remoteUser.username, self.startRecordingTime];
+    NSString *recordingName = [[NSString alloc] initWithFormat:@"%@_%@_%.1f.caf",self.localUser.username, self.remoteUser.username, self.startRecordingTime];
     NSLog(@"recording name is %@",recordingName);
     PFFile *newFile = [PFFile fileWithName:recordingName data:recordingData];
     
@@ -131,15 +147,94 @@
             NSLog(@"name: %@",newFile.name);
             NSLog(@"url: %@",newFile.url);
             
+            // Create an object to facilitate easy retrieval
+            PFObject *newRecording = [PFObject objectWithClassName:RECORDING_CLASSNAME];
+            [newRecording setObject:newFile forKey:@"recording"];
+            [newRecording setObject:self.localUser.username forKey:@"fromUser"];
+            [newRecording setObject:self.remoteUser.username forKey:@"toUser"];
+            
+            NSDate *currentTime = [[NSDate alloc]init];
+            [newRecording setObject:currentTime forKey:@"timestamp"];
+
+            [newRecording saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+                if (!error) {
+                    NSLog(@"Successfully saved block");
+                }
+                else {
+                    NSLog(@"Error saving meta-object for recording: %@ %@", error, [error userInfo]);
+                }
+            }];
         }
         else {
             NSLog(@"Error during save: %@ %@", error, [error userInfo]);
         }
     }];
+}
+
+- (IBAction)onTestButton:(id)sender {
+    NSLog(@"PLACEHOLDER FUNCTION - playback the all messages for this user in the last 10 minutes");
+    
+    // Test to see all users:
+    PFQuery *userQuery = [PFUser query];
+    userQuery.limit = 10;
+    [userQuery findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+        NSLog(@"Found %d nearby users",objects.count);
+        
+    }];
     
     
+    // Form a query for this user.
+    PFQuery *query = [PFQuery queryWithClassName:RECORDING_CLASSNAME];
+    PFUser *user = [PFUser currentUser];
+
+    NSDate *tenMinutesAgo = [[NSDate alloc] initWithTimeIntervalSinceNow:-600];
     
+    [query whereKey:@"toUser" equalTo:user.username];
+    [query whereKey:@"timestamp" greaterThan:tenMinutesAgo];
     
+    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+        self.latestRecordingArray = [NSMutableArray array];
+        if (objects.count > 0) {
+            for (PFObject *eachObject in objects) {
+                [self.latestRecordingArray addObject:eachObject];
+            }
+            NSLog(@"Found %d messages in the last 10 minutes.  Starting playback.",self.latestRecordingArray.count);
+            [self playbackAsBackground];
+        }
+        else {
+            NSLog(@"Found no recordings for the last 10 minutes");
+            return;
+        }
+    }];
+}
+
+-(void)playbackAsBackground {
+    if (!myQueue) {
+        myQueue = dispatch_queue_create("playback.background", NULL);
+    }
+    dispatch_async(myQueue, ^{
+        [self playback];
+    });
+}
+
+-(void)playback {
+    NSLog(@"Playing in background");
+    self.indexForPlayback = 0;
+    [self playNext];
+}
+
+-(void)playNext {
+    NSLog(@"Playing next entry");
+    if (self.indexForPlayback < self.latestRecordingArray.count) {
+        PFFile *thisFile = [self.latestRecordingArray[self.indexForPlayback] objectForKey:@"recording"];
+        NSData *playData = [thisFile getData];
+        NSLog(@"length of data is %d",playData.length);
+        audioPlayer = [[AVAudioPlayer alloc] initWithData:playData error:nil];
+        audioPlayer.delegate = self;
+        [audioPlayer play];
+        NSLog(@"Started playback");
+        self.indexForPlayback++;
+    }
 }
 
 //
@@ -150,6 +245,7 @@
 
 - (void)audioPlayerDidFinishPlaying:(AVAudioPlayer *)player successfully:(BOOL)flag {
     NSLog(@"Playback completed");
+    [self playNext];
 }
 
 
