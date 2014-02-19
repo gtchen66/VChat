80c0057e79c6e2202b13da4250821cdf000a4952
Joey Lin <jvlin@yahoo-inc.com>
2014-02-18 22:31:46 -0800

    fix for if displayName is not available

--- VChat/ContactsTableViewController.m
+++ VChat/ContactsTableViewController.m
@@ -32,9 +32,11 @@ NSString* const CONTACTS_KEY = @"contacts";
     return self;
 }
 
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    NSLog(@"ContactsTableViewController: viewDidLoad");
     self.title = @"Contacts";
     [self loadData];
     
@@ -67,8 +69,8 @@ NSString* const CONTACTS_KEY = @"contacts";
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
+    NSLog(@"numberOfRowsInSection");
     // Return the number of rows in the section.
-    NSLog(@"%@", self.contacts);
     return [self.contacts count];
 }
 
@@ -76,7 +78,6 @@ NSString* const CONTACTS_KEY = @"contacts";
 {
     ContactsCell *cell = [tableView dequeueReusableCellWithIdentifier:CONTACTS_CELL_IDENTIFIER];
     NSDictionary *userDictionary = [self.contacts objectAtIndex:indexPath.row];
-    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
     cell.nameLabel.text = userDictionary[@"displayName"];
     // determine whether to show "accept request" button, or show "request sent" status
     if ([userDictionary[@"status"] isEqualToString:@"accept request"]) {
@@ -135,14 +136,16 @@ NSString* const CONTACTS_KEY = @"contacts";
     
     NSDictionary *userDictionary = [self.contacts objectAtIndex:indexPath.row];
     PFQuery *query = [PFUser query];
-    PFUser *user = (PFUser *)[query getObjectWithId:userDictionary[@"objectId"]];
-    NSLog(@"Open conversation with %@",user.username);
-    NSLog(@"%@", user);
-    // open the chat window.
-    ChattingViewController *cvc = [[ChattingViewController alloc] init];
-    cvc.remoteUser = user;
+    [query getObjectInBackgroundWithId:userDictionary[@"objectId"] block:^(PFObject *object, NSError *error) {
+        PFUser *user = (PFUser *)object;
+        NSLog(@"Open conversation with %@",user.username);
+        // open the chat window.
+        ChattingViewController *cvc = [[ChattingViewController alloc] init];
+        cvc.remoteUser = user;
+        
+        [self.navigationController pushViewController:cvc animated:YES];
+    }];
     
-    [self.navigationController pushViewController:cvc animated:YES];
 }
  
 
@@ -151,7 +154,7 @@ NSString* const CONTACTS_KEY = @"contacts";
     PFUser *me = [PFUser currentUser];
     
     // Load contacts list from NSUserDefaults first, then find newer relations after latest time stamp.
-    // If NSUserDefaults is empty, fetch from Parse
+    // If NSUserDefaults is empty, fetch from Parse. Alternative is to use cached query
     
     // TODO - REFACTOR NSUserDeafult GET AND SET
     
@@ -160,62 +163,75 @@ NSString* const CONTACTS_KEY = @"contacts";
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
     NSArray *cachedContacts = [defaults arrayForKey:CONTACTS_KEY];
     if (!cachedContacts || cachedContacts == nil || cachedContacts.count == 0) {
+        
         // TODO - REFACTOR RELATIONS FETCH SINCE THIS IS DONE IN NEARBY LIST AS WELL
+        
         PFQuery *fromQuery = [PFQuery queryWithClassName:@"Friend"];
         [fromQuery whereKey:@"from" equalTo:me];
+        
         PFQuery *toQuery = [PFQuery queryWithClassName:@"Friend"];
         [toQuery whereKey:@"to" equalTo:me];
+        
         PFQuery *friendQuery = [PFQuery orQueryWithSubqueries:@[fromQuery, toQuery]];
+        
         // include the two user objects
         [friendQuery includeKey:@"to"];
         [friendQuery includeKey:@"from"];
+        
         [friendQuery findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
-//            self.friends = [[NSMutableArray alloc] initWithArray:objects];
             // save the objectId, display name, user name, and status (request sent, accept request, friends)
 //            NSLog(@"%@", objects);
-            for (PFObject *friend in objects) {
-                // if friending request was from other person, check if need to show "accept request" button
-                PFUser *to = [friend objectForKey:@"to"];
-                PFUser *from = [friend objectForKey:@"from"];
-//                NSLog(@"%@", to);
-//                NSLog(@"%@", from);
-                if ([me.objectId isEqualToString:to.objectId]) {
-                    if ([friend[@"status"] isEqualToString:@"pending"]) {
-                        NSDictionary *userDictionary = @{ @"objectId" : friend.objectId,
-                                                          @"username" : from.username,
-                                                          @"displayName" : from[@"displayName"],
-                                                          @"status" : @"accept request"
-                                                          };
-                        [self.contacts addObject:userDictionary];
+            if (!error) {
+                for (PFObject *friend in objects) {
+                    // if friending request was from other person, check if need to show "accept request" button
+                    PFUser *to = [friend objectForKey:@"to"];
+                    PFUser *from = [friend objectForKey:@"from"];
+                    NSLog(@"%@", to);
+                    NSLog(@"%@", from);
+                    if ([me.objectId isEqualToString:to.objectId]) {
+                        if ([friend[@"status"] isEqualToString:@"pending"]) {
+                            NSDictionary *userDictionary = @{ @"objectId" : friend.objectId,
+                                                              @"username" : from.username,
+                                                              @"displayName" : from[@"displayName"] ? from[@"displayName"] : from[@"username"],
+                                                              @"status" : @"accept request"
+                                                              };
+                            [self.contacts addObject:userDictionary];
+                        } else {
+                            NSDictionary *userDictionary = @{ @"objectId" : friend.objectId,
+                                                              @"username" : from.username,
+                                                              @"displayName" : from[@"displayName"] ? from[@"displayName"] : from[@"username"],
+                                                              @"status" : @"friends"
+                                                              };
+                            [self.contacts addObject:userDictionary];
+                        }
                     } else {
-                        NSDictionary *userDictionary = @{ @"objectId" : friend.objectId,
-                                                          @"username" : from.username,
-                                                          @"displayName" : from[@"displayName"],
-                                                          @"status" : @"friends"
-                                                          };
-                        [self.contacts addObject:userDictionary];
+                        NSLog(@"from id matches");
+                        if ([friend[@"status"] isEqualToString:@"pending"]) {
+                            NSLog((@"status from id pending"));
+                            NSDictionary *userDictionary = @{ @"objectId" : friend.objectId,
+                                                              @"username" : to.username,
+                                                              @"displayName" : to[@"displayName"] ? to[@"displayName"] : to[@"username"],
+                                                              @"status" : @"request sent"
+                                                              };
+                            [self.contacts addObject:userDictionary];
+                        } else {
+                            NSDictionary *userDictionary = @{ @"objectId" : friend.objectId,
+                                                              @"username" : to.username,
+                                                              @"displayName" : to[@"displayName"] ? to[@"displayName"] : to[@"username"],
+                                                              @"status" : @"friends"
+                                                              };
+                            [self.contacts addObject:userDictionary];
+                        }
                     }
-                } else {
-                    if ([friend[@"status"] isEqualToString:@"pending"]) {
-                        NSDictionary *userDictionary = @{ @"objectId" : friend.objectId,
-                                                          @"username" : to.username,
-                                                          @"displayName" : to[@"displayName"],
-                                                          @"status" : @"request sent"
-                                                          };
-                        [self.contacts addObject:userDictionary];
-                    } else {
-                        NSDictionary *userDictionary = @{ @"objectId" : friend.objectId,
-                                                          @"username" : to.username,
-                                                          @"displayName" : to[@"displayName"],
-                                                          @"status" : @"friends"
-                                                          };
-                        [self.contacts addObject:userDictionary];
-                    }
+                    NSLog(@"Contacts: ");
+                    NSLog(@"%@", self.contacts);
                 }
+            } else {
+                NSLog(@"%@", error);
             }
             
 //            NSLog(@"Contacts: ");
-//            NSLog(@"%@", self.contacts);
+            NSLog(@"%@", self.contacts);
             [self.tableView reloadData];
         }];
 
